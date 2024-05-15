d9c40873436655d85270d12a9a01604dcfc0e989
Katrin Winnen <katrin.riske@icloud.com>
2024-05-15 09:56:47 +0200

    added redirects

--- src/pages/RestaurantList.jsx
+++ src/pages/RestaurantList.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import EditForm from './EditForm'; // Make sure to import EditForm
 import './RestaurantList.css';
 
 function RestaurantList() {
